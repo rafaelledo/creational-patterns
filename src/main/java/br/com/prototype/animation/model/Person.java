1a8b2ff236c06b39bc1caf9eb55c540dd0279538
rafaelledo <rafaeloliveiraledo@gmail.com>
2022-10-12 23:41:47 -0300

    add prototype example

--- src/main/java/br/com/prototype/animation/model/Person.java
+++ src/main/java/br/com/prototype/animation/model/Person.java
@@ -1,11 +1,11 @@
 package br.com.prototype.animation.model;
 
-public class Person {
+public class Person implements Cloneable {
 	String line1;
 	String line2;
 	String line3;
 	String line4;
-	
+
 	public Person(String line1, String line2, String line3, String line4) {
 		this.line1 = line1;
 		this.line2 = line2;
@@ -20,7 +20,7 @@ public class Person {
 		System.out.println(line3);
 		System.out.println(line4);
 	}
-	
+
 	public void left() {
 		line1 = line1.substring(1);
 		line2 = line2.substring(1);
@@ -33,5 +33,10 @@ public class Person {
 		line2 = " " + line2;
 		line3 = " " + line3;
 		line4 = "_" + line4;
+	}
+
+	@Override
+	public Object clone() throws CloneNotSupportedException {
+		return super.clone();
 	}
 }
